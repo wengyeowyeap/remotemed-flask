1587676965543ea3c67edbb0a50bf6f1054161e7
wengyeowyeap <aquamagmayeow94@gmail.com>
2020-08-19 14:48:10 +0800

    changed username field to name

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@ from models.base_model import BaseModel
 import peewee as pw
 
 class User(BaseModel):
-    username = pw.CharField(unique=True, null=False)
+    name = pw.CharField(unique=True, null=False)
     email = pw.CharField(unique=True, null=False)
     password_hash = pw.TextField(null=False)
     password = None
