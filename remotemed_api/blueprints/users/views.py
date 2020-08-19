1e1d02070e761cdb486fcac056dff263c051ecda
wengyeowyeap <aquamagmayeow94@gmail.com>
2020-08-20 02:47:29 +0800

    updated signup -- handle possible multiple disease/role (take in data as array, add disease to response dynamically)

--- remotemed_api/blueprints/users/views.py
+++ remotemed_api/blueprints/users/views.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from models.user import User
 from models.user_disease import UserDisease
 from models.user_role import UserRole
+from models.disease import Disease
 from werkzeug.security import check_password_hash
 
 users_api_blueprint = Blueprint('users_api',
@@ -18,14 +19,51 @@ def sign_up():
     gender = request.json.get('gender')
     role = request.json.get('role')
 
-    if role == "1" or role == "1, 2": #if this is patient or patient/guardian
+    if ("1" in role) or ("1" in role and "2" in role) : #if this is patient or patient/guardian
         guardian = request.json.get('guardian')
         disease = request.json.get('disease') #value need to be number, example: 1->diabetes
         new_user = User(name = name, password = password, email = email, ic_number = ic_number, gender = gender, guardian = guardian)   
-        user_disease = UserDisease(disease=disease, user=new_user)
-        if new_user.save() and user_disease.save():
-            if role == "1":
-                user_role = UserRole(role=int(role), user=new_user)
+        if new_user.save():
+            for i in range(len(disease)):
+                user_disease = UserDisease(disease=int(disease[i-1]), user=new_user)
+                if user_disease.save():
+                    pass
+                else:
+                    response = {
+                        "message": "Some error occured, please try again",
+                        "status": "failed"
+                    }
+            if "1" in role and "2" in role:
+                user_role = UserRole(role=1, user=new_user)
+                user_role2 = UserRole(role=2, user=new_user)
+                if user_role.save() and user_role2.save():
+                    disease_name_list = []
+                    disease_list = Disease.select().join(UserDisease).where(UserDisease.user_id == new_user.id)
+                    for d in disease_list:
+                        disease_name_list.append(d.disease_name)
+                    response = {
+                        "message": f"Successfully created a user.",
+                        "status": "success",
+                        "user": {
+                            "id": new_user.id,
+                            "name": new_user.name,
+                            "email": new_user.email,
+                            "ic_number": new_user.ic_number,
+                            "gender": new_user.gender,
+                            "role": user_role.role.role_name,
+                            "role2": user_role2.role.role_name,
+                            "guardian": new_user.guardian
+                        }
+                    }
+                    for i in range(len(disease_name_list)):
+                        response["user"]["disease"+ str(i+1) ]= disease_name_list[i-1]
+                else:
+                    response = {
+                        "message": "Some error occured, please try again",
+                        "status": "failed"
+                    }
+            else:
+                user_role = UserRole(role=1, user=new_user)
                 if user_role.save():
                     response = {
                         "message": f"Successfully created a user.",
@@ -46,30 +84,7 @@ def sign_up():
                         "message": "Some error occured, please try again",
                         "status": "failed"
                     }
-            else:
-                user_role = UserRole(role=1, user=new_user)
-                user_role2 = UserRole(role=2, user=new_user)
-                if user_role.save() and user_role2.save():
-                    response = {
-                        "message": f"Successfully created a user.",
-                        "status": "success",
-                        "user": {
-                            "id": new_user.id,
-                            "name": new_user.name,
-                            "email": new_user.email,
-                            "ic_number": new_user.ic_number,
-                            "gender": new_user.gender,
-                            "role": user_role.role,
-                            "role2": user_role2.role,
-                            "disease": user_disease.disease,
-                            "guardian": new_user.guardian
-                        }
-                    }
-                else:
-                    response = {
-                        "message": "Some error occured, please try again",
-                        "status": "failed"
-                    }
+                
         else:
             error_message = ""
             for error in new_user.errors:
@@ -81,7 +96,7 @@ def sign_up():
     else: # not patient
         new_user = User(name = name, password_hash = password, email = email, ic_number = ic_number, gender = gender, guardian = None)
         if new_user.save():   
-            user_role = UserRole(role=int(role), user=new_user)     
+            user_role = UserRole(role=int(role[0]), user=new_user)     
             if user_role.save():
                 response = {
                     "message": f"Successfully created a user.",
@@ -92,7 +107,7 @@ def sign_up():
                         "email": new_user.email,
                         "ic_number": new_user.ic_number,
                         "gender": new_user.gender,
-                        "role": user_role.role,
+                        "role": user_role.role.role_name,
                     }
                 }
             else:
